bb486ce1bf9bbbffe7167447bba27d8cdb0eaa12
BelanovaMartinaTrain <belanova.m@gmail.com>
2023-11-17 10:34:51 +0100

    small change

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
             <h1 className="text-stone-200 font-poppins text-xl">Ahoj!</h1>
             <h1 className="text-stone-200 font-poppins mt-4">I&apos;m Martina</h1>
             <h1 className="text-stone-200 font-poppins">Frontend and Fullstack dev</h1>
-            <img src="./public/Foto.jpg" className="h-auto w-96 rounded-lg mt-8"/>
+            <img src="./Foto.jpg" className="h-auto w-96 rounded-lg mt-8"/>
         </section>
     )
   }
