55a1b6d3f750b2cd017f71f03739cee9d8c0aba0
BelanovaMartinaTrain <belanova.m@gmail.com>
2023-11-25 02:13:35 +0100

    small mobile screen adjust

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
   
     return (
-        <footer className="text-gray-400 flex justify-center align-middle mt-auto mb-4 p-4 max-h-16 font-noto text-sm max-[400px]:text-xs ">
+        <footer className="text-gray-400 flex justify-center align-middle mt-auto mb-4 p-4 max-h-16 font-noto text-sm max-[400px]:text-xs  ">
             <span className="font-dmsans font-bold text-sm max-[400px]:text-xs bg-black bg-opacity-60 p-1 rounded-md">&#169; 2023 Martina Belanova</span></footer>
     )
   }
