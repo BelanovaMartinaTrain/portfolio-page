08daaed20b7ae1ca2477306a4ea8c1ca148935e9
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-03-06 10:16:21 +0100

    to deploy

--- src/routes/Journey.jsx
+++ src/routes/Journey.jsx
@@ -9,7 +9,7 @@ function Journey() {
         async function fetchWeather() {
             
                 
-                    const weatherData = await fetchData("http://209.38.184.216:8080/api/planetary-k-3h");
+                    const weatherData = await fetchData("https://aurora-api.cloud/api/planetary-k-3h");
                 console.log(weatherData)
                
             
