f0b4efa3fb376b5f4c7aeb6b6554dd70dabdef03
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-04-09 20:23:17 +0200

    pokus opravit animaciu

--- src/routes/Connect.jsx
+++ src/routes/Connect.jsx
@@ -1,11 +1,12 @@
-
+import { SparklesBg } from "../components/ui/sparklesBg";
 
 function Connect() {
-  
     return (
-        <h1 className="text-stone-200">We can connect here</h1>
-    )
-  }
-  
-  export default Connect
-  
+        <>
+            <SparklesBg />
+            <h1 className="text-stone-200">We can connect here</h1>
+        </>
+    );
+}
+
+export default Connect;
--- src/routes/Journey.jsx
+++ src/routes/Journey.jsx
@@ -1,29 +1,12 @@
-import fetchData from "../api/fetchData";
-import { useEffect } from "react";
+import { SparklesPreview } from "../components/ui/sparklesTest";
 
 function Journey() {
-
-
-
-    useEffect(() => {
-        async function fetchWeather() {
-            
-                
-                    const weatherData = await fetchData("https://aurora-api.cloud/api/planetary-k-3h");
-                console.log(weatherData)
-               
-            
-        }
-
-        fetchWeather();
-        
-    }, []);
-
-  
     return (
-        <h1 className="text-stone-200">My jorney begins here</h1>
-    )
-  }
-  
-  export default Journey
-  
+        <>
+            <SparklesPreview />
+            <h1 className="text-stone-200">My jorney begins here</h1>
+        </>
+    );
+}
+
+export default Journey;
