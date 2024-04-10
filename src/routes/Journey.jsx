36ca53189d797001e4e43456889d160f41d5c9e9
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-04-11 00:05:19 +0200

    animated button

--- src/routes/Journey.jsx
+++ src/routes/Journey.jsx
@@ -3,7 +3,6 @@ import { Button } from "../components/ui/movingBorder";
 function Journey() {
     return (
         <>
-            <Button>Text</Button>
             <h1 className="text-stone-200">My jorney begins here</h1>
         </>
     );
