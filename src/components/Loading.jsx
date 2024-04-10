b3d3892dff6fa5e35b945474c4686f28230461d8
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-04-10 14:24:52 +0200

    loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,21 @@
 import { Loading } from "./LoadingGemini";
+import { SparklesCore } from "./ui/sparkles";
 
 export default function LoadingPage() {
     return (
         <>
             <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(70%_90%_at_center,transparent_40%,white)]"></div>
             <Loading />
+            <SparklesCore
+                id="tsparticlesfullpage"
+                background="transparent"
+                minSize={0.2}
+                maxSize={1.8}
+                particleDensity={150}
+                className="w-full h-full hidden"
+                particleColor="#2563EB"
+                speed={0.5}
+            />
         </>
     );
 }
