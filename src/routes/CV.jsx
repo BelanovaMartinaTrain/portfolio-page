d590a11275f7c468d2ebba451bc9496d8772185d
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-04-24 23:52:29 +0200

    email and cv parts

--- src/routes/CV.jsx
+++ src/routes/CV.jsx
@@ -19,7 +19,7 @@ export default function CV() {
         <>
             <div className="bg-gradient-to-r from-transparent via-slate-700 to-transparent my-2 print:my-0 h-[1px] w-full print:hidden " />
             <div className="w-full  grid justify-center">
-                <div className="flex gap-x-4 justify-end mt-2 print:hidden  ">
+                <div className="flex gap-x-4 justify-center md:justify-end mt-2 print:hidden  ">
                     <ButtonLink linkClass="" linkTo="/Martina_Belanova_CV_EN.pdf" spanClass={"text-sm"} iconType="download" download>
                         PDF
                     </ButtonLink>
@@ -31,16 +31,28 @@ export default function CV() {
                     </ButtonLink>
                 </div>
                 {data && (
-                    <div className="p-4 print:p-0 bg-slate-950 text-neutral-300 border border-blue-950/[0.5] rounded-lg print:border-none mt-4 print:mt-0 print:text-black max-w-6xl print:max-w-7xl w-full gap-4 text-normal print:text-xs grid grid-cols-3">
-                        <div className="col-span-2 p-4 print:p-0">
+                    <div
+                        className="p-0 md:p-4 print:p-0 bg-slate-950 text-neutral-300 border border-blue-950/[0.5] 
+                    rounded-lg print:border-none mt-4 print:mt-0 print:text-black max-w-6xl print:max-w-7xl w-full gap-4 
+                    text-normal print:text-xs grid lg:grid-cols-3 print:grid-cols-3"
+                    >
+                        <div className="col-span-2 p-4 pb-0 lg:pb-4 print:p-0">
                             <h1 className="text-4xl pb-2 text-neutral-50 print:text-sky-800 font-medium">
                                 {data.personal.first_name} {data.personal.last_name}
                             </h1>
                             <h2 className="print:text-sm">{data.personal.summary}</h2>
+                            <div className="print:border-none print:p-0  mt-5 lg:hidden print:hidden mb-6">
+                                <h4 className="print:text-xs">{data.personal.email}</h4>
+                                <h4 className="print:text-xs">{data.personal.phone}</h4>
+                                <a href={data.personal.url} className="print:text-xs">
+                                    https://martinabelanova.site
+                                </a>
+                                <img src="/qr_site.svg" alt="QR kod with link to martinabelanova.site" className="w-1/2 hidden print:block" />
+                            </div>
 
-                            <h3 className="mt-4 print:text-sm">Work experience:</h3>
+                            <h3 className="mt-4 mb-2 lg:mb-0 print:mb-0 print:text-sm">Work experience:</h3>
                             {data.work.map((job) => (
-                                <div key={job.company} className=" mb-2">
+                                <div key={job.company} className=" mb-5 lg:mb-2 print:mb-2">
                                     <h4 className="print:text-xs">{job.company}</h4>
                                     <h4 className="print:text-xs">{job.position}</h4>
 
@@ -58,9 +70,9 @@ export default function CV() {
                         </div>
 
                         <div className="col-span-1 flex p-1  print:p-0 ">
-                            <div className=" print:border print:border-l-slate-800/[0.5]  w-px h-full mr-4 bg-gradient-to-t from-transparent via-slate-700 to-transparent"></div>
+                            <div className="invisible lg:visible print:border print:border-l-slate-800/[0.5]  w-px h-full mr-4 bg-gradient-to-t from-transparent via-slate-700 to-transparent"></div>
                             <div className="mt-1">
-                                <div className="print:border-none  print:p-0    mb-6">
+                                <div className="print:border-none print:p-0  hidden lg:block print:block mb-6">
                                     <h4 className="print:text-xs">{data.personal.email}</h4>
                                     <h4 className="print:text-xs">{data.personal.phone}</h4>
                                     <a href={data.personal.url} className="print:text-xs">
@@ -68,7 +80,7 @@ export default function CV() {
                                     </a>
                                     <img src="/qr_site.svg" alt="QR kod with link to martinabelanova.site" className="w-1/2 hidden print:block" />
                                 </div>
-                                <div className="pt-4">
+                                <div className="print:pt-4 lg:pt-4">
                                     <h3 className="print:text-sm">Skills:</h3>
                                     {data.skills.map((skill, index) => (
                                         <div key={`skil-${index}`} className="pb-2">
